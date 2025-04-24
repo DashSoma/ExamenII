8f0b6d112b090fb75a9873e1da52e80086724ce4
dash <daobandoso@est.utn.ac.cr>
2025-04-24 11:25:47 -0600

    No se sabe
    No actualiza el nombre

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,27 +1,26 @@
 const Button = ({ onMostrar, offEliminar }) => {
-    return (
-      <div className="d-flex justify-content-center">
-        <button
-          type="button"
-          className="btn btn-outline-success m-2"
-          onClick={() => {
-            alert("Se presionó el botón. Se va a mostrar la imagen.");
-            onMostrar();
-          }}
-        >
-          Agregar 
-        </button>
-  
-        <button
-          type="button"
-          className="btn btn-outline-danger m-2"
-          onClick={offEliminar}
-        >
-          Eliminar 
-        </button>
-      </div>
-    );
-  };
-  
-  export default Button;
-  
+  return (
+    <div className="d-flex justify-content-center">
+      <button
+        type="button"
+        className="btn btn-outline-success m-2"
+        onClick={() => {
+          alert("Se presionó el botón. Se va a mostrar la imagen.");
+          onMostrar();
+        }}
+      >
+        Agregar triste
+      </button>
+
+      <button
+        type="button"
+        className="btn btn-outline-danger m-2"
+        onClick={offEliminar}
+      >
+        Eliminar
+      </button>
+    </div>
+  );
+};
+
+export default Button;
