fed58f5ce0591c0eb8d33edda12b22074c40b2e4
dash <daobandoso@est.utn.ac.cr>
2025-04-24 09:27:28 -0600

    Implementacion del input
    Se agrega el componente input de bootstrap el cual permitira agregar tareas

---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,15 @@
+function Pe({}) {
+    return (
+      <>
+<input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+<>
+);
+}
+
+export default Pe;
